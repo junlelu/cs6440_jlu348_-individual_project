53cb13ae1cfb0a9fe3fb4d157aad7281707504e5
Junle Lu <junlelu92@gmail.com>
2020-11-16 02:59:29 -0500

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Care Taker Monitoring App' });
+  res.render('index', { title: 'Caretaker Monitoring App' });
 });
 
 router.get('/patient_dashboard', function(req, res) {
