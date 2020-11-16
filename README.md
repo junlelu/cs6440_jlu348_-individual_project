3a5d15cd7d23fdfe57737e0e9f7c4f1b5ef09ad0
Junle Lu <junlelu92@gmail.com>
2020-11-16 02:49:40 -0500

    page skeletons

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 ## Reference
 
-The tutorial to create a node.js website
+The tutorial to create a node.js website\
 https://closebrace.com/tutorials/2017-03-02/the-dead-simple-step-by-step-guide-for-front-end-developers-to-getting-up-and-running-with-nodejs-express-and-mongodb
+
+## MongoDB
+**Installation**\
+https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/\
+brew tap mongodb/brew\
+brew install mongodb-community@4.4
+
+** start **\
+brew services start mongodb/brew/mongodb-community
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Care Taker Monitoring App' });
 });
 
-module.exports = router;
-/* GET Hello World page. */
 router.get('/patient_dashboard', function(req, res) {
   res.render('patient_dashboard', { title: 'Patient Dashboard' });
 });
+
+router.get('/setting', function(req, res) {
+  res.render('setting', { title: 'User Setting' });
+});
+
+module.exports = router;
